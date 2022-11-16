58f23436ea3e8d7784b1d10427b13b8f415d29b0
Sparsh Jaiswal <sparshjaiswal97@gmail.com>
2022-11-15 21:50:29 -0500

    Code Clean Up

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -23,22 +23,20 @@ using namespace std::chrono_literals;
 /* This example creates a subclass of Node and uses std::bind() to register a
  * member function as a callback from the timer. */
 
-class MinimalPublisher : public rclcpp::Node
-{
-public:
+class MinimalPublisher : public rclcpp::Node{
+ public:
   MinimalPublisher()
-  : Node("minimal_publisher"), count_(0)
-  {
+  : Node("minimal_publisher"), count_(0) {
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
   }
 
-private:
-  void timer_callback()
-  {
+ private:
+  void timer_callback() {
     auto message = std_msgs::msg::String();
-    message.data = "Hello, world! This is Sparsh's Publisher " + std::to_string(count_++);
+    message.data = "Hello, world! This is Sparsh's Publisher "
+                    + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
@@ -47,8 +45,7 @@ private:
   size_t count_;
 };
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
